55a8144d6a788ded81278b94d6f61190dca557e4
Stanley Su <stanleysu00@gmail.com>
2017-07-18 21:52:12 +1000

    implemented twitch user data retrieval

--- js/script.js
+++ js/script.js
@@ -1,3 +1,53 @@
 "use strict";
 
-console.log('desu');
+let defaultUsers = [
+  {
+    "name": "ESL_SC2",
+    "data": undefined
+  },
+  {
+    "name": "OgamingSC2",
+    "data": undefined
+  },
+  {
+    "name": "cretetion",
+    "data": undefined
+  },
+  {
+    "name": "freecodecamp",
+    "data": undefined
+  },
+  {
+    "name": "storbeck",
+    "data": undefined
+  },
+  {
+    "name": "habathcx",
+    "data": undefined
+  },
+  {
+    "name": "RobotCaleb",
+    "data": undefined
+  },
+  {
+    "name": "noobs2ninjas",
+    "data": undefined
+  }
+];
+
+function loadUserData(users) {
+	defaultUsers.forEach((user, i) => {
+		let url = 'https://wind-bow.gomix.me/twitch-api/users/' + user.name;
+		$.ajax({
+			type: 'GET',
+			url: url,
+			dataType: 'jsonp',
+			success: function(data) {
+				defaultUsers[i].data = data;
+			}
+		});
+	});
+}
+
+loadUserData(defaultUsers);
+
